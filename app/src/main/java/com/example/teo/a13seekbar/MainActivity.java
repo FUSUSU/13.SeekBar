3b5f8f2548b0f9494d00a2ac190813c41a557382
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-09 15:37:39 +0700

    Hoàn thành xữ lí

--- app/src/main/java/com/example/teo/a13seekbar/MainActivity.java
+++ app/src/main/java/com/example/teo/a13seekbar/MainActivity.java
@@ -3,15 +3,39 @@ package com.example.teo.a13seekbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+    SeekBar seekBar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Ánh xạ
+        seekBar1 = (SeekBar) findViewById(R.id.seekBar1);
+        seekBar1.setOnSeekBarChangeListener(this);//Bắt sự kiện barChangerListener
     }
 
+
+    //Phương thức xử lí sau khi bắt khi được thay đổi SeekBar.
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Toast.makeText(getApplicationContext(),"SeekBar progress: " + progress, Toast.LENGTH_SHORT).show();
+    }
+    //Báo hiệu bắt đầu chạm vào
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        Toast.makeText(getApplicationContext(), "SeekBar touch started!",Toast.LENGTH_SHORT).show();
+    }
+    //Bạo hiệu ngừng chạm
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        Toast.makeText(getApplicationContext(),"SeekBar touch stopped!",Toast.LENGTH_SHORT).show();
+    }
+
+    //Menu item
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
